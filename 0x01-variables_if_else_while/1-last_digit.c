f901b84b8b3fee6718f1687088dd63e76a43f835
Hannah Imo <preetyann21@gmail.com>
2022-10-14 14:19:21 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	}
 	else if (lastd == 0)
 	{ 
-		printf("Last digit if %d is %d and is 0\n", n, lastd);
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
@@ -28,5 +28,3 @@ int main(void)
 	}
 	return (0);
 }
-
-

d6d7e4412ca6be4847a1bf1fbf2ddb68244e988d
Hannah Imo <preetyann21@gmail.com>
2022-10-14 15:02:03 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 
 /**
- * * main -Prints a text according number
- * * Return: Always (Success)
- * */
+ * main -Prints a text according number
+ * Return: Always (Success)
+ * 
+ */
 
 int main(void)
 {
 	int n, lastd;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;

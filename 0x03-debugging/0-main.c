ccf682452ae6558a8383e66312a8bacdbb7e4452
Hannah Imo <preetyann21@gmail.com>
2022-10-20 16:28:29 -0700

    another

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests function for positive or negative
+ * main - tests function for postive or negative
  * Return: 0
  */
 
 int main(void)
 {
-int i;
+	int i;
+	i = 0;
+	positive_or_negative(i);
 
-i = 0;
-positive_or_negative(i);
-return (0);
+	return (0);
 }
+

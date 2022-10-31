3e5198ee348acd6bad2d832e3942c6d8248ee66f
Hannah Imo <preetyann21@gmail.com>
2022-10-31 09:32:18 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -19,11 +19,9 @@ i = 0;
 while (i < size) /*repeat numbers*/
 {
 /*sums of diagsums*/
-sum1 = sum1 + *(a + i * size + i);
-sum2 = sum2 + *(a + i * size + size - i - 1);
+sum1 = sum1 + *(a + i *size + i);
+sum2 = sum2 + *(a + i *size + size - i - 1);
 i++;
 }
 printf("%i, %i\n", sum1, sum2);
 }
-Footer
-© 2022 

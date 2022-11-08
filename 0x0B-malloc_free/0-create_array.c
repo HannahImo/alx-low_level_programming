2e98a8fbe857ba98c51d8198f3f1e3fde578c2f7
Hannah Imo <preetyann21@gmail.com>
2022-11-08 12:57:02 -0800

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,50 +2,26 @@
 
 #include <stdlib.h>
 
-
-
 /**
- *
- *  *create_array - creates an array of chars, and initializes it with a specific
- *
- *    * @size: the size the main function gave
- *
- *      * @c: the character we are given
- *
- *       * Return: pointer else return null
- *
- *         */
-
-
+ *create_array - creates an array of chars, and initializes it with a specific
+ * @size: the size the main function gave
+ * @c: the character we are given
+ * Return: pointer else return null
+ */
 
 char *create_array(unsigned int size, char c)
-
 {
-
-	char *ptr;
-
-	unsigned int night = 0;
-
-	if (size == 0)
-
-		return (NULL);
-
-	ptr = malloc(size * sizeof(char));
-
-	if (ptr == NULL)
-
-		return (NULL);
-
-	while (night < size)
-
-	{
-
-		ptr[night] = c;
-
-		night++;
-
-	}
-
-	return (ptr);
-
+char *ptr;
+unsigned int night = 0;
+if (size == 0)
+return (NULL);
+ptr = malloc(size * sizeof(char));
+if (ptr == NULL)
+return (NULL);
+while (night < size)
+{
+ptr[night] = c;
+night++;
+}
+return (ptr);
 }

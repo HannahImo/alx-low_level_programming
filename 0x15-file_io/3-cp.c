0e3268ec192e2e3e8d17d367667ddf6a0537658d
HannahImo <preetyann21@gmail.com>
2023-02-08 13:28:53 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,114 @@
 #include "main.h"
-/**
- * cp - copies src to desinations
- * @file_to: the destination file
- * @file_from: the source file
- * Return: integer
- */
+#include <stdio.h>
+#include <stdlib.h>
 
-int cp(char *file_to, char *file_from)
+/**
+ * check97 - checks for the correct number of arguments
+ * @argc: number of arguments
+ *
+ * Return: void
+ */
+void check97(int argc)
 {
-char *buffer[1024];
-int td, fd, fr, fw;
-int fc, ftc;
-fd = open(file_from, O_RDONLY);
-if (fd < 0)
-return (98);
-td = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (td < 0)
-return (99);
-fr = read(fd, buffer, 1024);
-if (fr < 0)
-return (98);
-while (fr > 0)
-{
-fw = write(td, buffer, fr);
-if (fw < 0)
-return (99);
-fr = read(fd, buffer, 1024);
-if (fr < 0);									return (98);									}
-fc = close(fd);
-if (fc < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fc);
-return (100);									}
-ftc = close(td);
-if (ftc < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ftc);
-return (100);
-}
-return (0);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 }
 
 /**
- * main - the main function
- * @ac: the argument count
- * @av: the argument vector
- * Return: always 0
+ * check98 - checks that file_from exists and can be read
+ * @check: checks if true of false
+ * @file: file_from name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
  */
-
-int main(int ac, char **av)
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
-int c;
-if (ac != 3)
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-c = cp(av[2], av[1]);
-switch (c)
-{
-case (98):
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-exit(98);
-case (99):
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-exit(99);
-case (100):
-exit(100);
-default:
-return (0);
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(98);
+	}
 }
+
+/**
+ * check99 - checks that file_to was created and/or can be written to
+ * @check: checks if true of false
+ * @file: file_to name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(99);
+	}
+}
+
+/**
+ * check100 - checks that file descriptors were closed properly
+ * @check: checks if true or false
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check100(int check, int fd)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
+	char buffer[1024];
+	mode_t file_perm;
+
+	check97(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
+	{
+		lenr = read(fd_from, buffer, 1024);
+		check98(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check99(lenw, argv[2], fd_from, fd_to);
+	}
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
+	return (0);
 }
